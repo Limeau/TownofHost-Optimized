dfccf33c3d7992d3ebb038646c0bb7581edc7b3e
Limeau <lime19844@gmail.com>
2026-04-27 17:50:32 -0700

    fix Artist

--- TOHO/Roles/Neutral/Artist.cs
+++ TOHO/Roles/Neutral/Artist.cs
@@ -34,16 +34,17 @@ namespace TOHO.Roles.Neutral
         {
             SetupRoleOptions(Id, TabGroup.NeutralRoles, CustomRoles.Artist);
             KillCooldown = FloatOptionItem.Create(Id + 10, GeneralOption.KillCooldown, new(0f, 180f, 2.5f), 30f, TabGroup.NeutralRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist])                
+                .SetValueFormat(OptionFormat.Seconds);
             PaintCooldown = FloatOptionItem.Create(Id + 11, "ArtistPaintCooldown", new(0f, 180f, 2.5f), 15f, TabGroup.NeutralRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
+                .SetValueFormat(OptionFormat.Seconds);
             CanVent = BooleanOptionItem.Create(Id + 12, GeneralOption.CanVent, true, TabGroup.NeutralRoles, false)
                 .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
             HasImpostorVision = BooleanOptionItem.Create(Id + 13, GeneralOption.ImpostorVision, true, TabGroup.NeutralRoles, false)
                 .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
             AbilityUses = IntegerOptionItem.Create(Id + 14, "ArtistAbilityUses", new(0, 15, 1), 5, TabGroup.NeutralRoles, false)
-                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist])
-                .SetValueFormat(OptionFormat.Times);
+                .SetParent(CustomRoleSpawnChances[CustomRoles.Artist]);
         }
 
         public override void Init()
@@ -83,101 +84,16 @@ namespace TOHO.Roles.Neutral
 
             if (PlayerSkinsPainted[killer.PlayerId].Contains(target.PlayerId))
             {
-                _ = new LateTask(() => { killer.SetKillCooldown(KillCooldown.GetFloat()); }, 0.1f, "Artist set kcd");
                 return true;
             }
-            else
-            {
-                return killer.CheckDoubleTrigger(target, () =>
-                {
-                    killer.RpcRemoveAbilityUse();
-                    SetSkin(target, PaintedOutfit);
-                    PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
-                    killer.SetKillCooldown(PaintCooldown.GetFloat());
-                    // Utils.NotifyRoles(SpecifySeer: killer, SpecifyTarget: target);
-                });
-            }
+            if (killer.CheckDoubleTrigger(target, () => { })) return true;
+            
+            killer.RpcRemoveAbilityUse();
+            target.SetNewOutfit(PaintedOutfit);
+            PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
+            killer.RpcGuardAndKill();
+            killer.SetKillCooldown(PaintCooldown.GetFloat());
+            return false;
         }
-
-        private void SetPainting(PlayerControl killer, PlayerControl target)
-        {
-            if (!PlayerSkinsPainted[killer.PlayerId].Contains(target.PlayerId))
-            {
-                if (!Camouflage.IsCamouflage)
-                {
-                    SetSkin(target, PaintedOutfit);
-                }
-
-                PlayerSkinsPainted[killer.PlayerId].Add(target.PlayerId);
-                killer.Notify(Utils.ColorString(Utils.GetRoleColor(CustomRoles.Artist), GetString("ArtistPaintedSkin")));
-
-                OriginalPlayerSkins[target.PlayerId] = Camouflage.PlayerSkins.GetValueOrDefault(target.PlayerId, null);
-                Camouflage.PlayerSkins[target.PlayerId] = PaintedOutfit;
-
-                SendRPC(killer.PlayerId, target.PlayerId);
-
-                killer.SetKillCooldown();
-            }
-            else
-            {
-                target.RpcMurderPlayer(killer);
-            }
-        }
-
-        public static bool IsPainting(byte seer, byte target)
-        {
-            return PlayerSkinsPainted.GetValueOrDefault(seer, new List<byte>()).Contains(target);
-        }
-
-        private static void SendRPC(byte playerId, byte targetId)
-        {
-            MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.SyncRoleSkill, SendOption.Reliable, -1);
-            writer.Write(playerId);
-            writer.Write(AbilityUses.GetInt());
-            writer.Write(targetId);
-            AmongUsClient.Instance.FinishRpcImmediately(writer);
-        }
-
-        public override void ReceiveRPC(MessageReader reader, PlayerControl NaN)
-        {
-            byte playerId = reader.ReadByte();
-            byte targetId = reader.ReadByte();
-            if (!PaintingTarget.ContainsKey(playerId))
-                PaintingTarget[playerId] = new List<byte>();
-
-            PaintingTarget[playerId].Add(targetId);
-        }
-
-        private void SetSkin(PlayerControl target, NetworkedPlayerInfo.PlayerOutfit outfit)
-        {
-            var sender = CustomRpcSender.Create(name: $"Artist.RpcSetSkin({target.Data.PlayerName})");
-
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetColor)
-                .Write(target.Data.NetId)
-                .Write((byte)outfit.ColorId)
-                .EndRpc();
-
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetHatStr)
-                .Write(outfit.HatId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetHatStr))
-                .EndRpc();
-
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetSkinStr)
-                .Write(outfit.SkinId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetSkinStr))
-                .EndRpc();
-
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetVisorStr)
-                .Write(outfit.VisorId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetVisorStr))
-                .EndRpc();
-
-            sender.AutoStartRpc(target.NetId, (byte)RpcCalls.SetPetStr)
-                .Write(outfit.PetId)
-                .Write(target.GetNextRpcSequenceId(RpcCalls.SetPetStr))
-                .EndRpc();
-
-            sender.SendMessage();
-        }        
     }
 }
