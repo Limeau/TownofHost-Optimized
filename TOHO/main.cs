081b6e439680b991d0adc3895ae8611b22be3461
Limeau <lime19844@gmail.com>
2026-05-02 19:42:46 -0700

    begin 2.9.0

--- TOHO/main.cs
+++ TOHO/main.cs
@@ -55,8 +55,8 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.Limeau.townofhostoptimized";
-    public const string PluginVersion = "2026.0502.280.00"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.8.0";
+    public const string PluginVersion = "2026.0516.290.00"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "2.9.0";
     public const string PluginDisplaySuffix = "";
     public const string SupportedVersionAU = "2026.3.31"; // Changed becasue Dark theme works at this version.
 
