a29ba307a59c71407eb8cfa5748b4c3681725ec3
lime1 <lime1@LIMEMACHINE>
2026-05-17 18:58:58 -0700

    begin development of v3.0.0

--- TOHO/main.cs
+++ TOHO/main.cs
@@ -55,8 +55,8 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.Limeau.townofhostoptimized";
-    public const string PluginVersion = "2026.0516.290.00"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.9.0";
+    public const string PluginVersion = "2026.0531.300.00"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginDisplayVersion = "3.0.0";
     public const string PluginDisplaySuffix = "";
     public const string SupportedVersionAU = "2026.3.31"; // Changed becasue Dark theme works at this version.
 
