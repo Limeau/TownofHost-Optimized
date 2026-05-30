5acd4a6e01b46f57edc8bd2ab72a8ff39807f1b3
lime1 <lime1@LIMEMACHINE>
2026-05-30 08:38:25 -0700

    sonic credits

--- TOHO/Roles/Neutral/Sonic.cs
+++ TOHO/Roles/Neutral/Sonic.cs
@@ -12,6 +12,8 @@ internal class Sonic : RoleBase
     public override CustomRoles ThisRoleBase => CustomRoles.Shapeshifter;
     public override Custom_RoleType ThisRoleType => Custom_RoleType.NeutralKilling;
     public override bool TOHORole => true;
+
+    public override string IdeaRole => "bearyyy_cocoa";
     //==================================================================\\
 
     public static OptionItem SonicKillCooldown;
