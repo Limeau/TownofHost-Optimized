f464ccb560f7a28a3b0387a3a0becb12b62d07c3
lime1 <lime1@LIMEMACHINE>
2026-06-03 19:10:56 -0700

    Legacy Valkyrie

--- TOHO/Roles/Crewmate/Valkyrie.cs
+++ TOHO/Roles/Crewmate/Valkyrie.cs
@@ -13,17 +13,58 @@ internal class Valkyrie : RoleBase
     public override bool TOHORole => true;
 
     public static OptionItem GhostKillCooldown;
-
+    public static OptionItem RevengeTime;
+    public static OptionItem Legacy;
 
     public override void SetupCustomOption()
     {
         SetupRoleOptions(Id, TabGroup.CrewmateRoles, CustomRoles.Valkyrie);
-        GhostKillCooldown = FloatOptionItem.Create(Id + 11, "ValkyrieRevengeTime", new(5f, 25f, 2.5f), 15f, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie])
+        GhostKillCooldown = FloatOptionItem.Create(Id + 11, "ValkyrieGhostCooldown", new(5f, 25f, 2.5f), 15f, TabGroup.CrewmateRoles, false).SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie])
             .SetValueFormat(OptionFormat.Seconds);
+        Legacy = BooleanOptionItem.Create(Id + 12, "UseLegacyValkyrie", false, TabGroup.CrewmateRoles, false)
+            .SetParent(CustomRoleSpawnChances[CustomRoles.Valkyrie]);
+        RevengeTime = FloatOptionItem.Create(Id + 13, "ValkyrieRevengeTime", new(5f, 25f, 2.5f), 15f, TabGroup.CrewmateRoles, false)
+            .SetParent(Legacy)
+            .SetValueFormat(OptionFormat.Seconds);
+        
     }
+
+    public override bool CanUseKillButton(PlayerControl pc)
+    {
+        return true;
+    }
+
+    public override void SetKillCooldown(byte id)
+    {
+        Main.AllPlayerKillCooldown[id] = 1;
+    }
+
     public override bool OnCheckMurderAsTarget(PlayerControl killer, PlayerControl target)
     {
-        new LateTask(() =>
+        if (Legacy.GetBool())
+        {
+            target.RpcChangeRoleBasis(CustomRoles.ImpostorTOHO);
+            target.RpcRandomVentTeleport();
+            killer.RpcGuardAndKill();
+            target.SetKillCooldownV3(1);
+            
+            new LateTask(() =>
+            {
+                if (killer.IsAlive()) target.RpcMurderPlayer(target);
+                else
+                {
+                    target.RpcSetCustomRole(CustomRoles.Valkyrie);
+                    target.RpcChangeRoleBasis(CustomRoles.Valkyrie);
+                    target.Notify(Translator.GetString("ValkyrieSuccess"));
+                }
+            }, RevengeTime.GetFloat(), "Valkyrie Revenge");
+            
+            killer.Notify(Translator.GetString("ValkyrieHide"));
+            target.Notify(string.Format(Translator.GetString("ValkyrieRevenge"), RevengeTime.GetFloat(), killer.GetRealName().RemoveHtmlTags()));
+            
+            return false;
+        }
+        else new LateTask(() =>
         {
             target.RpcSetCustomRole(CustomRoles.ValkyrieB, true);
         }, 1f, "Valkyrie Set Role");
