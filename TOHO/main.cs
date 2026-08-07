d24e4c9e33348d951c87ed849f6792ecd6c5dfe9
Limeau <148590907+Limeau@users.noreply.github.com>
2026-08-07 00:25:09 -0700

    emergency release for critical starlight bug

--- TOHO/main.cs
+++ TOHO/main.cs
@@ -60,7 +60,7 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
 
     public const string PluginGuid = "com.Limeau.townofhostoptimized";
-    public const string PluginVersion = "2026.0809.360.00"; // YEAR.MMDD.VERSION.CANARYDEV
+    public const string PluginVersion = "2026.0807.360.00"; // YEAR.MMDD.VERSION.CANARYDEV
     public const string PluginDisplayVersion = "3.6.0";
     public const string PluginDisplaySuffix = "";
     public const string SupportedVersionAU = "2026.3.31"; // Changed becasue Dark theme works at this version.
